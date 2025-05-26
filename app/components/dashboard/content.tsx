f33bbeb2844cfe80fef9052dba7ec5356737abe6
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-05-26 15:09:03 -0400

    Estructura

---
+++ app/components/dashboard/content.tsx
@@ -0,0 +1,11 @@
+const Content = () => {
+  return (
+    <div className="p-4 border border-dashed border-gray-300 rounded-md">
+      <p className="text-gray-500 text-center">
+        Componente Dashboard (Placeholder)
+      </p>
+    </div>
+  );
+};
+
+export default Content;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,11 +3,13 @@
 import { useState } from "react";
 import Navbar from "@/app/components/dashboard/navbar";
 import Footer from "@/app/components/dashboard/footer";
+import Content from "@/app/components/dashboard/content";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
       <Navbar />
+      <Content />
       <Footer />
     </div>
   );
